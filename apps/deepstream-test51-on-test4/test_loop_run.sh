8fdc79fe149c25bef7c67459bf882b4973ed6825
shao <shaojun@example.com>
2022-05-12 10:44:59 +0800

    update test_loop_run

--- apps/deepstream-test51-on-test4/test_loop_run.sh
+++ apps/deepstream-test51-on-test4/test_loop_run.sh
@@ -1,4 +1,4 @@
 for n in  {0..100000}
 do 
-    python3 deepstream_test_51.py -i file:///home/eow/Videos/mini_yang_demo_ele.mp4 --no-display --no-rtsp-output 
+    python3 deepstream_test_51.py -i file:///home/eow/Videos/mini_yang_demo_ele.mp4 --no-display --no-output-rtsp
 done
