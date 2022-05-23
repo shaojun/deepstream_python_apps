d995f12e91c039026cb3ad4b03218965e6407c5d
shao <shaojun@example.com>
2022-05-23 17:25:40 +0800

    implement the gastank detection and upload

--- apps/deepstream-test51-on-test4/test_loop_run.sh
+++ apps/deepstream-test51-on-test4/test_loop_run.sh
@@ -1,4 +1,4 @@
 for n in  {0..100000}
 do 
-    python3 deepstream_test_51.py -i file:///home/eow/Videos/mini_yang_demo_ele.mp4 --no-display --no-output-rtsp
+    python3 deepstream_test_51.py -i file:///home/kevin/Downloads/yang_office.mp4
 done
